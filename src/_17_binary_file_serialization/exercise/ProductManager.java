2cc765ee6143f2bfbabd2f36b36fbb79005ed311
C0222G1-NguyenQuangPhuc <phucprokoten@gmail.com>
2022-04-24 20:46:20 +0700

    fix bai tap ngay binary file 24/4

--- src/_17_binary_file_serialization/exercise/ProductManager.java
+++ src/_17_binary_file_serialization/exercise/ProductManager.java
@@ -6,55 +6,55 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ProductManager {
+    public static final String sourceFile = "src/_17_binary_file_serialization/exercise/product_list.txt";
     public static List<Product> listProduct = new ArrayList<>();
     public static Scanner sc = new Scanner(System.in);
 
     public void display() throws IOException {
-        listProduct = (List<Product>) ReadAndWriterBinaryFile.readBinaryFile("src/_17_binary_file_serialization/exercise/product_list.txt");
+        listProduct = (List<Product>) ReadAndWriterBinaryFile.readBinaryFile(sourceFile);
         for (Product item : listProduct) {
             System.out.println(item);
         }
     }
 
-    public void addNewProduct() {
+    public void addNewProduct() throws IOException {
+        listProduct = (List<Product>) ReadAndWriterBinaryFile.readBinaryFile(sourceFile);
         try {
-            while (true){
-                System.out.println("Enter id: ");
-                int id = Integer.parseInt(sc.nextLine());
-                System.out.println("Enter name product: ");
-                String name = sc.nextLine();
-                System.out.println("Enter branch: ");
-                String brand = sc.nextLine();
-                System.out.println("Enter price: ");
-                double price = Double.parseDouble(sc.nextLine());
-                System.out.println("Describe product: ");
-                String describe = sc.nextLine();
-                Product newProduct = new Product(id, name, brand, price, describe);
-                listProduct.add(newProduct);
-                ReadAndWriterBinaryFile.writeBinaryFile("src/_17_binary_file_serialization/exercise/product_list.txt",listProduct );
-                System.out.println("Add new successful");
-                break;
-            }
-        }catch (NumberFormatException e){
+            System.out.println("Enter id: ");
+            int id = Integer.parseInt(sc.nextLine());
+            System.out.println("Enter name product: ");
+            String name = sc.nextLine();
+            System.out.println("Enter branch: ");
+            String brand = sc.nextLine();
+            System.out.println("Enter price: ");
+            double price = Double.parseDouble(sc.nextLine());
+            System.out.println("Describe product: ");
+            String describe = sc.nextLine();
+            Product newProduct = new Product(id, name, brand, price, describe);
+            listProduct.add(newProduct);
+            ReadAndWriterBinaryFile.writeBinaryFile("src/_17_binary_file_serialization/exercise/product_list.txt", listProduct);
+            System.out.println("Add new successful");
+        } catch (NumberFormatException e) {
             System.out.println("Retry");
         }
     }
 
-    public void findById(){
+    public void findById() throws IOException {
+        listProduct = (List<Product>) ReadAndWriterBinaryFile.readBinaryFile(sourceFile);
         System.out.println("Enter Id of Product: ");
         try {
             int id = Integer.parseInt(sc.nextLine());
             boolean check = false;
             for (Product item : listProduct) {
-                if (item.getId() == id){
+                if (item.getId() == id) {
                     System.out.println(item.toString());
                     check = true;
                 }
             }
-            if (!check){
+            if (!check) {
                 System.out.println("Not Found");
             }
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             System.out.println("Please enter right Id Product");
         }
     }
---
+++ src/_19_string_regex/practice/CrawlSongExample.java
@@ -0,0 +1,15 @@
+package _19_string_regex.practice;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CrawlSongExample {
+
+}
+
